926f2adc380d1ded42d8a02fc243e6c7254eb8cf
UltraFerous <jandoga31@gmail.com>
2023-11-15 22:46:43 -0500

    Added SQL injection protection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,21 +1,27 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
+  user: 'labber',
+  password: 'labber',
   host: 'localhost',
   database: 'bootcampx'
 });
 
-pool.query(`
-    SELECT DISTINCT teachers.name as teacher_name, cohorts.name as cohort_name
-    FROM assistance_requests
-    JOIN students ON students.id = student_id
-    JOIN cohorts ON students.cohort_id = cohorts.id
-    JOIN teachers ON teachers.id = assistance_requests.teacher_id
-    WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-    ORDER BY teacher_name
-    `)
+const limit = process.argv[2] || 'JUL02';
+// Store all potentially malicious values in an array.
+const values = [limit];
+
+const queryString = `
+SELECT DISTINCT teachers.name as teacher_name, cohorts.name as cohort_name
+FROM assistance_requests
+JOIN students ON students.id = student_id
+JOIN cohorts ON students.cohort_id = cohorts.id
+JOIN teachers ON teachers.id = assistance_requests.teacher_id
+WHERE cohorts.name = $1
+ORDER BY teacher_name
+`;
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.cohort_name}: ${user.teacher_name}`);
